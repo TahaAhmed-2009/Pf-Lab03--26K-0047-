481f02cbc43828a39d1e720b906cbf28de1f52c1
TahaAhmed-2009 <tahaahmedirfan05@gmail.com>
2026-09-13 14:31:55 +0000

    Add Escape sequence task.c

---
+++ Escape sequence task.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+//NAME
+printf("Name : \"Taha Ahmed\"\n");
+//ROLL NUMBER
+printf("Roll number : 26K-0047\n");
+//SECTION
+printf("Section : BSAI\t1-B\n");
+    return 0;
+}
