af251f9f2aad8cc26dfe9baefeb14df967d2eb3f
TahaAhmed-2009 <tahaahmedirfan05@gmail.com>
2026-09-13 14:22:22 +0000

    Add Division remainder.c

---
+++ Division remainder.c
@@ -0,0 +1,15 @@
+                   //Division-Remainder-Qoutient Task
+#include <stdio.h>
+int main(){
+int num1;
+int num2;
+printf("Enter the First number\n");
+scanf("%d",&num1);
+printf("Enter the Second number\n");
+scanf("%d",&num2);
+printf("Following are the Qoutient and the Remainder when First number is divided by the Second number\n");
+printf("Remainder : %d\n",num1%num2);
+printf("Qoutient : %d\n",num1/num2);
+
+    return 0;
+}
